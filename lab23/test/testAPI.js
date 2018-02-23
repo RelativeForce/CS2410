774b09fc6ab9728dcf250bde28d30b1cb719a3bf
RelativeForce <joshiceddy@hotmail.com>
2018-02-23 10:16:40 +0000

    Started Lab 4

--- lab23/test/testAPI.js
+++ lab23/test/testAPI.js
@@ -7,6 +7,47 @@ const apiRoot = 'http://localhost:3000/';
 
 describe('Test API', function() {
 
+	/*
+	 * A variable to store the server in. We need this so the 'before' block can
+	 * share the server it creates with the 'after' block. The 'after' block
+	 * needs access to the server so that it can close it after the tests have
+	 * finished executing.
+	 */
+	var server;
+	before(function(done) {
+		/* Code to start the server */
+		/* It needs to call the done callback when it is finished */
+
+		const express = require('express');
+		const status = require('http-status');
+
+		var app = express();
+
+		app.get('/', function(req, res) {
+			res.send('Hello, World!');
+		});
+
+		app.post('/', function(req, res) {
+			res.sendStatus(status.OK);
+		});
+
+		const port = 3000;
+		server = app.listen(port, function() {
+			done()
+		});
+	});
+
+	after(function() {
+		/* Code to stop the server */
+		/*
+		 * As it happens, this code will be synchronous. No need for a done
+		 * callback
+		 */
+
+		server.close();
+
+	});
+
 	/* Note that the function passed to 'it' has parameter 'done' */
 	it('Test test', function(done) {
 
@@ -30,7 +71,7 @@ describe('Test API', function() {
 
 	it('GET request returns text "Hello, World!".', function(done) {
 		request.get(apiRoot).end(function(err, res) {
-			
+
 			expect(res.text).to.equal('Hello, World!');
 			expect(res.statusCode).to.equal(status.OK);
 			expect(err).to.not.be.an('error');
@@ -40,7 +81,7 @@ describe('Test API', function() {
 
 	it('POST request is not allowed', function(done) {
 		request.post(apiRoot).end(function(err, res) {
-			
+
 			expect(err).to.not.be.an('error');
 			expect(res.statusCode).to.equal(status.OK);
 			done();
---
+++ lab4/lab4.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Lab 4 tasks </title>
+</head>
+
+<body>
+	<h1>Lab 4 tasks</h1>
+
+	<!-- Task 1: String-->
+	<!-- write your solution to Task 1 here -->
+	<div class="section">
+		<h2>Task 1 : String</h2>
+		
+	
+	
+	</div>
+
+	<!-- Task 2: Array and image-->
+	<!-- write your solution to Task 2 here -->
+	<div class="section">
+		<h2>Task 2 : Array and image</h2>
+
+		
+		
+	</div>	
+
+	<!-- Task 3: Function definition dayinmonth  -->
+	<!-- write your solution to Task 3 here -->
+	<div class="section">
+		<h2>Task 3 : Function definition</h2>
+		
+	
+	
+	</div>
+	
+
+	
+	<!-- Task 4: Favorite Artists from a File (Files) -->
+	<!-- write your solution to Task 4 here -->
+	<div class="section">
+		<h2>Task 4: My Favorite Artists from a file</h2>
+		
+
+		
+	</div>
+	
+	<!-- Task 6: Directory operations -->
+	<!-- write your solution to Task 6 here -->
+	<div class="section">
+		<h2>Task 6 : Directory operations</h2>
+
+
+		
+	</div>
+
+	<!-- Task 6 optional: Directory operations -->
+	<!-- write your solution to Task 6 optional here -->
+	<div class="section">
+		<h2>Task 6 optional: Directory operations optional</h2>
+	
+	
+	
+	</div>
+	</div
+
+
+	
+    <!-- Task 5: including external files -->
+	<!-- write your solution to Task 5 here -->
+	<div class="section">
+		<h2>Task 5: including external files</h2>
+			
+			
+	</div>
+
+</body>
+</html>
