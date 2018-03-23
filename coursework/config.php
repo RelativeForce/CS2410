aaf50a5c7fb73d45864312398f27580dd9343de3
RelativeForce <joshiceddy@hotmail.com>
2018-03-23 13:31:11 +0000

    Changed Coursework to node.js

--- coursework/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-$host = "localhost";
-$dbname = "AstonEvents"
-$username = "root";
-$password = "";
-
-$db = new PDO("mysql:dbname=" . $dbname . ";host=" . $host, $username, $password);
-
-?>
--- coursework/connectdb.php
+++
@@ -1,7 +0,0 @@
-<?php 
-
-include config.php;
-
-$db=new PDO("mysql:dbname=cs2410;host=localhost",$username,$password);
-
-?>
