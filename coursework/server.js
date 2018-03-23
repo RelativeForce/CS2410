c737b0a381ed67a28052e8c0e2079b3709f0c10a
RelativeForce <joshiceddy@hotmail.com>
2018-03-23 18:06:26 +0000

    Landing page set up

--- coursework/server.js
+++ coursework/server.js
@@ -2,13 +2,20 @@ const express = require('express');
 const status = require('http-status');
 const path = require('path')
 const port = 3000;
-
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/CS2410/coursework/login-student', function(req, res) {
+	res.sendFile('C:/xampp/htdocs/CS2410/coursework/login-student.html');
+});
+
+app.get('/CS2410/coursework/login-organiser', function(req, res) {
+	res.sendFile('C:/xampp/htdocs/CS2410/coursework/login-organiser.html');
+});
+
 app.get('/CS2410/coursework/', function(req, res) {
-	res.sendFile('C:/xampp/htdocs/CS2410/coursework/login.html');
+	res.sendFile('C:/xampp/htdocs/CS2410/coursework/landing.html');
 });
 
 app.post('/CS2410/coursework/', function(req, res) {
