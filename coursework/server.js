517445a55973fcc8a794fbbe45e07cac5578b3d5
RelativeForce <joshiceddy@hotmail.com>
2018-03-23 16:38:27 +0000

    Coursework server outputs login page

--- coursework/server.js
+++ coursework/server.js
@@ -1,18 +1,17 @@
 const express = require('express');
 const status = require('http-status');
-
+const port = 3000;
 
 var app = express();
 
-app.get('/', function(req, res) {
-	res.send('Hello, World!');
+app.get('/CS2410/coursework/', function(req, res) {
+	res.sendFile('C:/xampp/htdocs/CS2410/coursework/login.html');
 });
 
-app.post('/', function(req, res) {
+app.post('/CS2410/coursework/', function(req, res) {
 	res.sendStatus(status.OK);
 });
 
-const port = 3000;
 app.listen(port, function() {
 	console.log('Listening on port ' + port);
 });
