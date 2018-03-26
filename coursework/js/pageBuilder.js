4dd6ee189afd270eb25a495bac5174d73a1f4293
RelativeForce <joshiceddy@hotmail.com>
2018-03-26 16:03:24 +0100

    First step to dynamic web pages

---
+++ coursework/js/pageBuilder.js
@@ -0,0 +1,103 @@
+function head(title) {
+
+	var head = '';
+	head += '<head>';
+	head += '	<meta charset="utf-8" />';
+	head += '	<title>' + title + '</title>';
+	head += '	<link href="/default.css" rel="stylesheet" type="text/css" />';
+	head += '	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>';
+	head += '	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" />';
+	head += '	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>';
+	head += '</head>';
+
+	return head;
+
+}
+
+function navbar(navElements) {
+
+	var nav = '';
+	nav += '<nav class="navbar navbar-inverse navbar-static-top">';
+	nav += '	<div class="container-fluid">';
+	nav += '		<div class="navbar-header">';
+	nav += '			<button type="button" class="navbar-toggle collapsed pull-right" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">';
+	nav += '				<span class="sr-only">Toggle navigation</span>';
+	nav += '				<span class="icon-bar"></span>';
+	nav += '				<span class="icon-bar"></span>';
+	nav += '				<span class="icon-bar"></span>';
+	nav += '			</button>';
+	nav += '			<img class="pull-left navbar-brand" src="/aston-logo.png" style="padding-top: 10px; padding-bottom: 10px; height: 50px" />';
+	nav += '		</div>';
+	nav += '		<div id="navbar" class="navbar-collapse collapse">';
+	nav += '			<ul class="nav navbar-nav">';
+
+	for (i = 0; i < navElements.length; i++) {
+		nav += navElements[i];
+	}
+
+	nav += '</ul></div></div></nav>';
+
+	return nav;
+
+}
+
+function login() {
+
+	var login = '';
+
+	login += '<li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Login<span class="caret"></span></a>';
+	login += '	<ul class="dropdown-menu">';
+	login += '		<li><a href="coursework/login-student">Student Login</a></li>';
+	login += '		<li><a href="coursework/login-organiser">Organiser Login</a></li>';
+	login += '	</ul>';
+	login += '</li>';
+
+	return login;
+
+}
+
+function body(navbar, content) {
+
+	var body = '';
+	body += '<body>';
+	body += navbar;
+	body += content;
+	body += '</body>';
+
+	return body;
+
+}
+
+function page(head, body) {
+
+	var document = '';
+
+	document += '<!DOCTYPE html>';
+	document += '<html lang="en">';
+
+	document += head;
+	document += body;
+
+	document += '</html>';
+
+	return document;
+}
+
+module.exports = {
+	navbar : function(navElements) {
+		return navbar(navElements);
+	},
+	head : function(title) {
+		return head(title);
+	},
+	body : function(navbar, content) {
+		return body(navbar, content);
+	},
+	page : function(head, body) {
+		return page(head, body);
+	},
+	login : function() {
+		return login();
+	}
+
+};
--- coursework/server.js
+++ coursework/server.js
@@ -1,21 +1,34 @@
+// Import required packages
 const express = require('express');
 const status = require('http-status');
-const path = require('path')
+const path = require('path');
 const port = 3000;
 const app = express();
+const builder = require('./js/pageBuilder');
 
+// Add /public as the static assets folder
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/CS2410/coursework/login-student', function(req, res) {
-	res.sendFile('C:/xampp/htdocs/CS2410/coursework/login-student.html');
+		
+	var login = builder.login();
+	var navbar = builder.navbar([login]);
+	var head = builder.head("Student Login");
+	var body = builder.body(navbar, "no content");
+	var page = builder.page(head, body);
+	
+	res.writeHead(200, {'Content-Type':'text/html'});
+	res.write(page);
+	res.end();
+	
 });
 
 app.get('/CS2410/coursework/login-organiser', function(req, res) {
-	res.sendFile('C:/xampp/htdocs/CS2410/coursework/login-organiser.html');
+	res.sendFile('C:/xampp/htdocs/CS2410/coursework/components/login-organiser.html');
 });
 
 app.get('/CS2410/coursework/', function(req, res) {
-	res.sendFile('C:/xampp/htdocs/CS2410/coursework/landing.html');
+	res.sendFile('C:/xampp/htdocs/CS2410/coursework/components/landing.html');
 });
 
 app.post('/CS2410/coursework/', function(req, res) {
