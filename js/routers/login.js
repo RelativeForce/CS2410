8e392cb7662e6c6c9c76cb7eab907909ba66f8ec
RelativeForce <joshiceddy@hotmail.com>
2018-04-22 10:59:57 +0100

    Documented login.js

--- js/routers/login.js
+++ js/routers/login.js
@@ -1,6 +1,16 @@
+/**
+ * This is a Router which handles requests to the 'login' end point. This end
+ * point logs a user in and signs users up to the site.
+ * 
+ * @author Joshua Eddy 159029448
+ * @since 2018-04-22
+ */
+
+// NPM Modules
 const express = require('express');
 const router = express.Router();
 
+// My Modules
 const db = require('./../modules/dbHelper');
 const builder = require('./../modules/pageBuilder');
 const sessions = require('./../modules/sessionHelper');
@@ -8,157 +18,203 @@ const cookieName = sessions.cookieName;
 const MD5 = require('./../modules/MD5');
 const misc = require('./../modules/misc');
 
-
 /**
- * Processes the GET requests to the 'login' end point which will respond with
- * the login page.
+ * Processes the GET requests to / and will respond with the login page.
  * 
  * @param request
- *            Cannot contain a valud session cookie.
+ *            The GET request to the server.
  * @param response
- *            The login page.
+ *            The response that will be sent to the client.
  * @returns undefined
  */
 function get(request, response) {
-	build_login(request, response, "");
+	build_login(response, "");
 }
 
 /**
- * Processes POST requests to the 'login' end point which will take a request to
- * log a client in or sign a client up. If the request is valid the client will
- * be redirected to the home page.
+ * Processes POST requests to / and will log a client in or sign a client up. If
+ * the request is valid the client will be redirected to the home page.
  * 
  * @param request
- *            That contains all the details of the login credentials. Passwords
- *            should be hashed before they are sent.
+ *            The POST request that was sent to the server.
  * @param response
- *            A Redirect to the home page or the login page with the error.
+ *            The response that will be sent to the client.
  * @returns undefined
  */
 function post(request, response) {
-	
+
 	// If the request is to log in.
 	if (request.body.status === "login") {
 		login(request, response);
-	} 
-	
+	}
+
 	// If the request is to sign up
 	else if (request.body.status === "signup") {
 		signup(request, response);
 	}
-	
+
 	// Invalid request.
-	else{
+	else {
 		response.redirect('/');
 	}
 }
 
+/**
+ * Handles a POST request to / that specifed that the user wants to login.
+ * 
+ * @param request
+ *            The POST request that was sent to the server.
+ * @param response
+ *            The response that will be sent to the client.
+ * @returns undefined
+ */
 function login(request, response) {
 
-	// Sterilise the username and password
-	var email = misc.encodeHTML(request.body.email);
-	var password = misc.encodeHTML(request.body.password);
+	// If the emaila nd password are populated
+	if (request.body.email && request.body.password) {
 
-	db.each(
-		"SELECT * FROM Users WHERE email = ?",
-		[email], 
-		function(user) {
+		var email = misc.encodeHTML(request.body.email);
+		var password = misc.encodeHTML(request.body.password);
+
+		/*
+		 * Iterate over all the users with the specifed email. There should only
+		 * be one as the user email is unique.
+		 */
+		db.each("SELECT * FROM Users WHERE email = ?", [ email ], function(user) {
 
 			var salted = MD5.hash(password + user.salt);
-	
+
+			// If the password created from the request fields is the same as
+			// the password stored in the database.
 			if (salted === user.password) {
-	
+
 				var token = sessions.uniqueToken();
-	
+
 				// If the session is added redirct the client.
 				if (sessions.addSession(token, user)) {
 					response.redirect('/');
-				} else {
-					// Session alread exists.
+				}
+				// Session already exists.
+				else {
+
 					var error = builder.error("Session exists elsewhere. Please sign out in the other location.");
-					build_login(request, response, error);
+					build_login(response, error);
 				}
 			} else {
-	
+
 				// Invalid password
 				var error = builder.error("Password is incorrect.");
-				build_login(request, response, error);
+				build_login(response, error);
 			}
 
-		}, 
-		function(count) {
-		
+		}, function(count) {
+
 			// Invalid email
 			if (count == 0) {
 				var error = builder.error("<strong>" + email + "</strong> is not a valid email.");
-				build_login(request, response, error);
+				build_login(response, error);
 			}
-		}
-	);
+		});
+	} else {
+
+		// Email or passowrd missing.
+		var error = builder.error("<strong>email</strong> and/or <strong>password</strong> is not a defined.");
+		build_login(response, error);
+	}
 
 }
 
+/**
+ * 
+ * Handles a POST request to / that specifed that the user wants to signup.
+ * 
+ * @param request
+ *            The POST request that was sent to the server.
+ * @param response
+ *            The response that will be sent to the client.
+ * @returns undefined
+ */
 function signup(request, response) {
 
-	var salt = generateSalt();
-	var password = misc.encodeHTML(request.body.password);
-	var saltedPassword = MD5.hash(password + salt);
-	
-	var newUser = {
-			
+	// if all the signup feilds are populated.
+	if (request.body.email && request.body.name && request.body.dob && request.body.telephone && request.body.password) {
+
+		var salt = generateSalt();
+		var password = misc.encodeHTML(request.body.password);
+		var saltedPassword = MD5.hash(password + salt);
+
+		var newUser = {
+
 			"email" : misc.encodeHTML(request.body.email),
 			"name" : misc.encodeHTML(request.body.name),
 			"dob" : misc.encodeHTML(request.body.dob),
 			"organiser" : "false",
-			"picture"  : "none",
-			"password"  : saltedPassword,
+			"picture" : "none",
+			"password" : saltedPassword,
 			"salt" : salt,
 			"telephone" : misc.encodeHTML(request.body.telephone)
-					
+
 		};
 
-	db.each(
-		"SELECT * FROM Users WHERE email = ?",
-		[email], 
-		function(row) {
-			// Count elements
-		},
-		function(count) {
-	
-			// User does not exist already
-			if (count == 0) {
-	
-				db.run(
-					"INSERT INTO Users('email', 'name', 'dob', 'organiser' ,'picture','password', 'salt', 'telephone') VALUES (?, ?, ?, ?, ?, ?, ?, ?);",
-					[newUser.email, newUser.name, newUser.dob, newUser.organiser, newUser.picture, newUser.password, newUser.salt, newUser.telephone]
-				);
-	
-				console.log("User created: " + newUser);
-	
-				var token = sessions.uniqueToken();
-	
-				sessions.addSession(token, newUser);
-				response.redirect('/');
-	
-			} else {
-	
-				var error = builder.error("A user with Email: <strong>" + newUser.email + "</strong> already exists.");
-				build_login(request, response, error);
-	
+		// Check that the email doesn not exist.
+		db.each(
+			"SELECT * FROM Users WHERE email = ?", 
+			[ newUser.email ], 
+			function(user) {
+				// Count users
+			}, function(count) {
+
+				// User does not exist already
+				if (count == 0) {
+
+					db.run(
+						"INSERT INTO Users('email', 'name', 'dob', 'organiser' ,'picture','password', 'salt', 'telephone') VALUES (?, ?, ?, ?, ?, ?, ?, ?);", 
+						[newUser.email, newUser.name, newUser.dob, newUser.organiser, newUser.picture, newUser.password, newUser.salt, newUser.telephone ]
+					);
+
+					console.log("User created: " + newUser);
+
+					var token = sessions.uniqueToken();
+
+					sessions.addSession(token, newUser);
+					response.redirect('/');
+
+				} else {
+
+					var error = builder.error("A user with Email: <strong>" + newUser.email + "</strong> already exists.");
+					build_login(response, error);
+
+				}
 			}
-		}
-	);
+		);
+
+	} else {
+
+		var error = builder.error("<strong>email</strong> and/or " + "<strong>password</strong> and/or "
+				+ "<strong>name</strong> and/or " + "<strong>dob</strong> and/or "
+				+ "<strong>telephone</strong> is not a defined.");
+		build_login(response, error);
+
+	}
 }
 
+/**
+ * Builds the login page.
+ * 
+ * @param response
+ *            The response that will be sent to the client.
+ * @param error
+ *            An error box that will be added to the top of the form.
+ * @returns undefined
+ */
+function build_login(response, error) {
 
-function build_login(request, response, error) {
-
-	// Builds the student login page
+	// Builds the login page
 	misc.buildPage('login', function(content) {
 
 		var home = builder.navbarLink("/", "Home");
 		var search = builder.navbarLink("/search", "Search Events");
-		
+
 		var navbar = builder.navbar([ home, search ]);
 		var head = builder.head("Login");
 		var body = builder.body(navbar, error + content);
@@ -170,7 +226,12 @@ function build_login(request, response, error) {
 
 }
 
-
+/**
+ * Generates a random alpha numeric string that will be used as the salt in the
+ * database.
+ * 
+ * @returns A random alpha numeric string.
+ */
 function generateSalt() {
 
 	const saltLength = 30;
