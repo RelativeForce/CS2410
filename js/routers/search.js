a2ec74ca6e2e2ed056c2f3f3a7124cd2a30b3502
RelativeForce <joshiceddy@hotmail.com>
2018-04-22 15:42:11 +0100

    Documented search.js

--- js/routers/search.js
+++ js/routers/search.js
@@ -1,44 +1,58 @@
+/**
+ * This is a Router which handles requests to the 'search' end point. This end
+ * point displays the serch page.
+ * 
+ * @author Joshua Eddy 159029448
+ * @since 2018-04-22
+ */
+
+// NPM Modules
 const express = require('express');
 const router = express.Router();
 
+// My Modules
 const db = require('./../modules/dbHelper');
 const builder = require('./../modules/pageBuilder');
 const sessions = require('./../modules/sessionHelper');
 const misc = require('./../modules/misc');
 const cookieName = sessions.cookieName;
 
+/**
+ * Handles GET requests to / and will respond with a search event page that may
+ * be filtered based on the request query.
+ * 
+ * @param request
+ *            The GET request sent to the server.
+ * @param response
+ *            The response that will be sent to the client.
+ * @returns undefined
+ */
 function get(request, response) {
 
-	// Check for the session cookie and wherther it is active.
+	// Get the session token.
 	var sessionToken = request.cookies[cookieName];
 
-	// If there is a active session build the nav bar with the user
-	// options
+	// If the user is signed in
 	if (sessions.validSession(sessionToken)) {
+
+		// Extend the session
 		sessions.extend(sessionToken, response);
 
-		var email = sessions.getEmail(sessionToken);
+		var user = sessions.getDetails(sessionToken);
 
-		db.each("SELECT * FROM Users WHERE email = ?", [ email ],
-				function(user) {
+		var logout = builder.navbarLink("/logout", "Logout");
+		var profile = builder.navbarLink("/profile?email=" + user.email, "My Profile");
+		var newEvent = builder.navbarLink("/organise", "Orgainse Event");
+		var home = builder.navbarLink("/", "Home");
+		var myEvents = builder.navbarLink("/events", "My Events");
 
-					var logout = builder.navbarLink("/logout", "Logout");
-					var profile = builder.navbarLink("/profile?email="
-							+ user.email, "My Profile");
-					var newEvent = builder.navbarLink("/organise",
-							"Orgainse Event");
-					var home = builder.navbarLink("/", "Home");
-					var myEvents = builder.navbarLink("/events", "My Events");
+		var navbar = builder.navbar(
+			(user.organiser === 'true') ? 
+			[ home, newEvent, myEvents, profile, logout ] : 
+			[ home, profile, logout ]
+		);
 
-					var navbar = builder.navbar((user.organiser === 'true') ? [
-							home, newEvent, myEvents, profile, logout ] : [
-							home, profile, logout ]);
-
-					filterEvents(request, response, navbar, email);
-
-				}, function(count) {
-					// Do nothing
-				});
+		filterEvents(request.query, response, navbar, user.email);
 
 	} else {
 
@@ -47,12 +61,18 @@ function get(request, response) {
 
 		var navbar = builder.navbar([ home, login ]);
 
-		filterEvents(request, response, navbar, "");
+		filterEvents(request.query, response, navbar, "");
 	}
 
 }
 
-function getDefaultSearch() {
+/**
+ * Retreves the default search filter for the search page which is no filter and
+ * ordered by date.
+ * 
+ * @returns Filter
+ */
+function getDefaultFilter() {
 
 	return {
 
@@ -68,17 +88,24 @@ function getDefaultSearch() {
 
 }
 
-function getDateSearch(request) {
+/**
+ * Reteives the search that has been filtered by date.
+ * 
+ * @param query
+ *            The query that from the request to the server.
+ * @returns Filter
+ */
+function getDateFilter(query) {
 
 	// The params for the date filter
-	var from = request.query.from;
-	var to = request.query.to;
+	var from = query.from;
+	var to = query.to;
 
 	// The validity of the params
 	var validFrom = (from) && (from !== "");
 	var validTo = (to) && (to !== "");
 
-	// If the user has specified a from and to
+	// If the user has specified a 'from' and 'to' dates
 	if (validFrom && validTo) {
 
 		return {
@@ -93,7 +120,9 @@ function getDateSearch(request) {
 
 		};
 
-	} else if (validFrom) {
+	}
+	// If the user has specifed just a 'from' date.
+	else if (validFrom) {
 
 		return {
 
@@ -107,7 +136,9 @@ function getDateSearch(request) {
 
 		};
 
-	} else if (validTo) {
+	}
+	// If the user has specifed just a 'to' date.
+	else if (validTo) {
 
 		return {
 
@@ -121,18 +152,28 @@ function getDateSearch(request) {
 
 		};
 
-	} else {
-		return getDefaultSearch();
+	}
+	// If the user has specifed no dates.
+	else {
+		return getDefaultFilter();
 	}
 
 }
 
-function getTypeSearch(request) {
+/**
+ * Reteives the search that has been filtered by event type.
+ * 
+ * @param query
+ *            The query that from the request to the server.
+ * @returns filter
+ */
+function getTypeFilter(query) {
 
-	var type = request.query.type;
+	var type = query.type;
 
 	var validType = (type) && (type !== "");
 
+	// If there is a valid type specifed filter by that.
 	if (validType) {
 
 		return {
@@ -147,17 +188,25 @@ function getTypeSearch(request) {
 		};
 
 	} else {
-		return getDefaultSearch();
+		return getDefaultFilter();
 	}
 
 }
 
-function getPopularitySearch(request) {
+/**
+ * Reteives the search that has been filtered by popularity.
+ * 
+ * @param query
+ *            The query that from the request to the server.
+ * @returns Filter
+ */
+function getPopularityFilter(query) {
 
-	var minimum = request.query.minimum;
+	var minimum = query.minimum;
 
 	var validMinimum = (minimum) && (minimum !== "");
 
+	// If the user has specifed a minimum popularity
 	if (validMinimum) {
 
 		return {
@@ -171,7 +220,9 @@ function getPopularitySearch(request) {
 
 		};
 
-	} else {
+	}
+	// If the user ha not specifed a minumum popularity.
+	else {
 
 		return {
 
@@ -187,62 +238,53 @@ function getPopularitySearch(request) {
 
 }
 
-function filterEvents(request, response, navbar, email) {
+/**
+ * Filters the events based on the request query and the uses them to build the
+ * serach page results.
+ * 
+ * @param query
+ *            The query that from the request to the server.
+ * @param response
+ *            The response that will be sent to the user.
+ * @param navbar
+ *            The navbar that will be displayed at the top of the page.
+ * @param email
+ *            The email of the current user. If no user is signed in it will be
+ *            blank.
+ * @returns undefined
+ */
+function filterEvents(query, response, navbar, email) {
 
-	var search = getDefaultSearch();
+	var search = getFilter(query);
 
-	if (request.query.filter) {
+	db.collect(search.queryText, search.params, function(event) {
 
-		// The filter is by date
-		if (request.query.filter === "Date") {
-			search = getDateSearch(request);
-		}
-		// Filter by Type
-		else if (request.query.filter === "Type") {
-			search = getTypeSearch(request);
-		}
-		// Filter by Popularity
-		else if (request.query.filter === "Popularity") {
-			search = getPopularitySearch(request);
-		}
-	}
+		event.hasLiked = false;
+		return event;
 
-	db.collect(search.queryText, search.params, function(rawEvent) {
-
-		return {
-			"name" : rawEvent.name,
-			"id" : rawEvent.event_id,
-			"location" : rawEvent.location,
-			"time" : rawEvent.time,
-			"type" : rawEvent.type,
-			"organiser" : rawEvent.organiser,
-			"popularity" : rawEvent.popularity,
-			"hasLiked" : false
-		};
 	}, function(events) {
 
 		// If there is a user signed in then check if they have liked any of the
 		// events that will be displayed.
 		if (email !== "") {
 
-			db.each("SELECT * FROM Interest WHERE student_email = ?",
-					[ email ], function(row) {
+			db.each("SELECT * FROM Interest WHERE student_email = ?", [ email ], function(row) {
 
-						// Check if the current event is in the events list and
-						// if so
-						// then it has been liked by the user.
-						for (var index = 0; index < events.length; index++) {
-							var current = events[index];
+				/*
+				 * Check if the current event is in the events list and if so
+				 * then it has been liked by the user.
+				 */
+				for (var index = 0; index < events.length; index++) {
+					var current = events[index];
 
-							if (row.event_id === current.id) {
-								current.hasLiked = true;
-							}
-						}
+					if (row.event_id === current.id) {
+						current.hasLiked = true;
+					}
+				}
 
-					}, function(interestCount) {
-						build_search(response, events, navbar, search.filter,
-								true)
-					});
+			}, function(interestCount) {
+				build_search(response, events, navbar, search.filter, true)
+			});
 
 		} else {
 			build_search(response, events, navbar, search.filter, false)
@@ -252,6 +294,53 @@ function filterEvents(request, response, navbar, email) {
 
 }
 
+/**
+ * Reterevs the filter that will be applied to the events in the search table
+ * based on the specifed query.
+ * 
+ * @param query
+ *            The query that from the request to the server.
+ * @returns Filter
+ */
+function getFilter(query) {
+
+	var search = getDefaultFilter();
+
+	if (query.filter) {
+
+		// The filter is by date
+		if (query.filter === "Date") {
+			search = getDateFilter(query);
+		}
+		// Filter by Type
+		else if (query.filter === "Type") {
+			search = getTypeFilter(query);
+		}
+		// Filter by Popularity
+		else if (query.filter === "Popularity") {
+			search = getPopularityFilter(query);
+		}
+	}
+
+	return search;
+
+}
+
+/**
+ * Builds the search page.
+ * 
+ * @param response
+ *            The response that will be sent to the client.
+ * @param events
+ *            The array of events that will be displayed on the search page.
+ * @param navbar
+ *            The navbar that will appear at the top of the page.
+ * @param filter
+ *            The filter that resulted in the array of events.
+ * @param signedIn
+ *            Whether of not the user is signed in or not.
+ * @returns undefined
+ */
 function build_search(response, events, navbar, filter, signedIn) {
 
 	var eventsTable = builder.eventsTable(events, "Results", signedIn);
