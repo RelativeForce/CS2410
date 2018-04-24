e18b7de7cf460c141b939b128e6afac8afa25656
RelativeForce <joshiceddy@hotmail.com>
2018-04-24 12:23:57 +0100

    updated login validator

--- public/scripts/loginValidator.js
+++ public/scripts/loginValidator.js
@@ -79,7 +79,7 @@ function validateSignup() {
 
 	var phoneRegex = /^[0-9]{11}$/;
 
-	if (!telephone.toString().test(phoneRegex)) {
+	if (telephone.toString().test(phoneRegex) !== true) {
 		alert("Telephone number must be valid.");
 		return false;
 	}
